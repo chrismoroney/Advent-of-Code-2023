78dfed97264f9b0782cb73bcd124048c79cf9630
chrismoroney <christopher.moroney1@gmail.com>
2023-12-09 10:58:35 -0800

    pausing, adding current changes

--- Q4/code.py
+++ Q4/code.py
@@ -1,2 +1,18 @@
+def process_ticket(game):
+    parts = game.split('|')
+    if parts:
+        parts[0] = parts[0].split(':')[1].strip()
+    print(parts)
+
+
+def parse_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                process_ticket(line)
+    except FileNotFoundError:
+        print(f"file doesn't exist ", {file_path})
+
 if __name__ == "__main__":
-    print()
+    file_path = './input_sample.txt'
+    parse_file(file_path)
