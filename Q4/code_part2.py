b02cf3087966628e795a11e993c900e5e65861b3
chrismoroney <christopher.moroney1@gmail.com>
2023-12-09 16:02:45 -0800

    part 2 complete

--- Q4/code_part2.py
+++ Q4/code_part2.py
@@ -1,9 +1,3 @@
-def count_score(wins):
-    if wins == 0:
-        return 0
-    else:
-        return 2 ** (wins - 1)
-
 def count_winning_nums(winning_nums, have_nums):
     wins = 0
     for i in have_nums:
@@ -14,27 +8,42 @@ def count_winning_nums(winning_nums, have_nums):
 def process_ticket(game):
     parts = game.split('|')
 
-    _, string_num = parts[0].split(':')
-    _ = _.replace('Card', '').strip()
+    game_num, string_num = parts[0].split(':')
+    game_num = int(game_num.replace('Card', '').strip())
 
     winning_nums = {int(num): False for num in string_num.split()}
     have_nums = [int(num) for num in parts[1].split()]
     
-    return winning_nums, have_nums
+    return game_num, winning_nums, have_nums
 
+
+def count_total_cards(card_map):
+    total = 0
+    for cards in card_map.values():
+        total += cards
+    return total
 
 def parse_file(file_path):
-    total_scores = 0
+    # create a map that tracks which card and how many of that card there are
+    num_copies_of_cards = {}
     try:
         with open(file_path, 'r') as file:
             for line in file:
-                winning_nums, have_nums = process_ticket(line)
+                game_num, winning_nums, have_nums = process_ticket(line)
                 wins = count_winning_nums(winning_nums, have_nums)
-                points = count_score(wins)
-                total_scores += points
+
+                num_copies_of_cards[game_num] = num_copies_of_cards.get(game_num, 0) + 1
+                
+                if wins != 0:
+                    for i in range(1, wins + 1):
+                        num_copies_of_cards[game_num+i] = num_copies_of_cards.get(game_num+i, 0) + num_copies_of_cards[game_num]
+                
     except FileNotFoundError:
         print(f"file doesn't exist ", {file_path})
-    return total_scores
+    
+    total = count_total_cards(num_copies_of_cards)
+    return total
+    
 
 if __name__ == "__main__":
     file_path = './input.txt'
