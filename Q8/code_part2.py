cc280d3ae34fc1f940e7434dbbb992ba2f2f3b66
chrismoroney <christopher.moroney1@gmail.com>
2023-12-18 22:41:03 -0800

    part2 finished

--- Q8/code_part2.py
+++ Q8/code_part2.py
@@ -1,14 +1,46 @@
-def count_steps(instruction, map_of_nodes, num_steps, current):
-    for char in instruction:
-        if current == 'ZZZ':
-            return num_steps
-        else:
-            num_steps += 1
-            if char == 'L':
-                current = map_of_nodes[current][0]
-            else:
-                current = map_of_nodes[current][1]
-    return count_steps(instruction, map_of_nodes, num_steps, current)
+from math import gcd
+
+def find_lcm(num_steps_in_cycle):
+    lcm = num_steps_in_cycle.pop()
+    for num in num_steps_in_cycle:
+        lcm = lcm * num // gcd(lcm, num)
+    return lcm
+
+# Def inspired and recieved help from HyperNeutrino: https://www.youtube.com/watch?v=_nnxLcrwO_U
+def count_steps_in_cycle(instruction, map_of_nodes, starting_nodes):
+    all_cycles = []
+    for node in starting_nodes:
+        cycle = []
+        current_steps = instruction
+        step_count = 0
+        first_z = None
+
+        while True:
+            while step_count == 0 or not node.endswith('Z'):
+                step_count += 1
+                if current_steps[0] == 'L':
+                    node = map_of_nodes[node][0]
+                else:
+                    node = map_of_nodes[node][1]
+                current_steps = current_steps[1:] + current_steps[0]
+
+            cycle.append(step_count)
+            if first_z is None:
+                first_z = node
+                step_count  = 0
+            elif node == first_z:
+                break
+        all_cycles.append(cycle)
+    
+    return [cycle[0] for cycle in all_cycles]
+            
+
+def find_starting_nodes(map_of_nodes):
+    starting_nodes = []
+    for map in map_of_nodes:
+        if map[2] == 'A':
+            starting_nodes.append(map)
+    return starting_nodes
 
 def create_map_of_nodes(nodes):
     map_of_nodes = {}
@@ -42,5 +74,7 @@ def parse_file(file_path):
 if __name__ == '__main__':
     instruction, nodes = parse_file('./input.txt')
     map_of_nodes = create_map_of_nodes(nodes)
-    count_num_steps = count_steps(instruction, map_of_nodes, 0, 'AAA')
-    print(count_num_steps)
+    starting_nodes = find_starting_nodes(map_of_nodes)
+    num_steps_in_cycle= count_steps_in_cycle(instruction, map_of_nodes, starting_nodes)
+    lcm = find_lcm(num_steps_in_cycle)
+    print(lcm)
