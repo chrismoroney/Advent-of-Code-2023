c48a4d48d1f8353ab925815d785d908b26367238
Chris <christopher.moroney1@gmail.com>
2023-12-20 21:15:11 -0800

    find starting position

--- Q10/code_part1.py
+++ Q10/code_part1.py
@@ -1,2 +1,19 @@
+def find_start(grid):
+    for x, row in enumerate(grid):
+        for y, char in enumerate(row):
+            if char == 'S':
+                return (x, y)
+    
+
+
+def parse_file(file_path):
+    grid = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            grid.append(list(line.strip()))
+    return grid
+
 if __name__ == '__main__':
-    print()
+    grid = parse_file('./input_sample_2.txt')
+    start = find_start(grid)
+    print(start)
